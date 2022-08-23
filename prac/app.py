d163b871abefb8196ef9597717ed997893e1e19e
KoungHun Ko <sand9688@naver.com>
2022-08-23 17:39:05 +0900

    flask_prac

---
+++ prac/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+nextId = 4
+topics = [
+    {'id': 1,'title': 'html', 'body': 'html is ...'},
+    {'id': 2,'title': 'css', 'body': 'css is ...'},
+    {'id': 3,'title': 'javascript', 'body': 'javascript is ...'}
+]
+
+def template(contents, content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href='/'>WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+            <ul>
+                <li><a href="/create/">create</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>hello, WEB')
+
+
+
+@app.route('/read/<int:id>/')
+def read(id):
+
+    title = ''
+    body = ''
+
+    for topic in topics:
+        if id == topic['id']:
+            title = topic['title']
+            body = topic['body']
+            break
+
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+@app.route('/create/', methods=['GET','POST'])
+def create():
+    if request.method =='GET':
+        content_1 = '''
+        <form action="/create/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea placeholder="body" name="body"></textarea></p>
+                <p><input type="submit" value="create"></P>
+        </form>
+        '''
+        return template(getContents(), content_1)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic={'id': nextId, 'title': title, 'body': body}
+        topics.append(newTopic)
+        url = '/read/'+str(nextId)+'/'
+        nextId += 1
+        return redirect(url)
+
+
+app.run(debug=True)
